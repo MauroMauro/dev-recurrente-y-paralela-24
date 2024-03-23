fc412c6d07bf313abf1bee7676e47238728a4ab8
MauroMauro <blur.benitez@gmail.com>
2024-03-23 08:12:25 -0300

    done ex1

---
+++ 1 - Java - Leer CSV/ej1LeerArchivoCSV/main.java
@@ -0,0 +1,42 @@
+package ej1LeerArchivoCSV;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class main {
+	public static final String SEPARATOR=";";
+	public static final String QUOTE="\"";
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = null;
+		
+		try {
+			br= new BufferedReader(new FileReader("src/ej1LeerArchivoCSV/vial.csv"));
+			String line = br.readLine();
+			while (null!=line) {
+				String [] fields = line.split(SEPARATOR);
+				System.out.println(Arrays.toString(fields));
+				
+				//fields = removeTrailingQuotes(fields);
+				System.out.println(Arrays.toString(fields));
+				
+				line = br.readLine();	
+			}
+				
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if(null!=br) {
+				br.close();
+			}
+		}
+
+		
+		
+		
+	}
+
+}
