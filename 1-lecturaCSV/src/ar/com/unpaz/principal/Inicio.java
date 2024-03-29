ae942a06b349ae0780064c178c8ad5bcc36ab711
MauroMauro <blur.benitez@gmail.com>
2024-03-29 10:31:04 -0300

    implemented CLI

--- 1-lecturaCSV/src/ar/com/unpaz/principal/Inicio.java
+++ 1-lecturaCSV/src/ar/com/unpaz/principal/Inicio.java
@@ -2,6 +2,7 @@ package ar.com.unpaz.principal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import ar.com.unpaz.model.Vial;
 import ar.com.unpaz.servicio.LeerArchivoVialServicio;
@@ -10,16 +11,34 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		LeerArchivoVialServicio servicio = new LeerArchivoVialServicio();
-		System.out.print("***************************");
-		System.out.print("    Programa de lectura    ");
-		System.out.print("***************************");
 		
-		List<Vial> lista = new ArrayList<Vial>();
+		Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+	    int opcion;
+	    
+	    List<Vial> lista = new ArrayList<Vial>();
 		lista = servicio.getListViales();
+	    
+		while (!salir) {
+			System.out.println("=======  Lectura CSV Vialidad   =======");
+			System.out.println("1- Listar todos los registros");
+			System.out.println("2- Actualizar registro");
+			System.out.println("3- Borrar registro");
+			System.out.println("3- Crear registro");
+			System.out.println("4- Salir");
+
+			System.out.println("Escriba una de las opciones");
+			opcion = sn.nextInt();
+			
+			switch(opcion) {
+			case 1:
+				for (Vial vial : lista) {
+					System.out.println(vial.toString());
+				}
+			}
+		}//fin del while
 		
-		for (Vial vial : lista) {
-			System.out.println(vial.toString());
-		}
+		
 
 	}
 
